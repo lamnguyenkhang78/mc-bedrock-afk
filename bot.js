8518e705ab7f844708d0e07cdd382042834fbd34
lamnguyenkhang78 <lamnguyenkhang78@gmail.com>
2026-01-18 18:46:29 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const { createClient } = require('bedrock-protocol');
 function join() {
+  console.log('Trying to join...');
   createClient({
     host: 'congdongbanthan.mcsh.io',
     port: 30207,
     username: 'BotAFK'
-  }).on('kick', join).on('error', join);
+  })
+    .on('connect', () => console.log('BotAFK connected'))
+    .on('kick', (reason) => console.log('Kicked:', reason))
+    .on('error', (e) => console.log('Error:', e.message))
+    .on('close', () => { console.log('Closed, reconnect...'); setTimeout(join, 5000); });
 }
 join();
